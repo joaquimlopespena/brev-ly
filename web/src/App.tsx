04ff3bb4c64810997cc94030c54c9e9d4765d571
Joaquimlp <joaquimlp2013@gmail.com>
2025-08-16 13:34:09 -0300

    FIx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,22 @@
+import { Route, Routes } from 'react-router-dom'
 import Content from './components/Content'
 import Logo from './components/Logo'
 import Index from './pages/Index'
+import NotFound from './pages/NotFound'
+import Redirect from './pages/Redirect'
 //import Redirect from './pages/Redirect'
 //import NotFound from './pages/NotFound'
 
 function App() {
     return (
-        <>
-            <Content>
-                <Logo />
-                <Index />
-                {/* <Redirect /> */}
-                {/* <NotFound /> */}
-            </Content>
-        </>
+        <Content>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/:shortUrl" element={<Redirect />} />
+                <Route path="*" element={<NotFound />} />
+                <Route path="/not-found" element={<NotFound />} />
+            </Routes>
+        </Content>
     )
 }
 
--- web/src/pages/Index.tsx
+++ web/src/pages/Index.tsx
@@ -1,12 +1,16 @@
 import CardForm from "../components/CardForm";
 import CardList from "../components/CardList";
+import Logo from "../components/Logo";
 import Main from "../components/Main";
 
 export default function Index() {
     return (
-        <Main>
-            <CardForm />
-            <CardList />
-        </Main>
+        <>
+            <Logo />
+            <Main>
+                <CardForm />
+                <CardList />
+            </Main>
+        </>
     )
 }
